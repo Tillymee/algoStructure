c3d0134eb7dda07557068ae35230c87206402a07
tillymee <tilly.zech70@gmail.com>
2024-09-24 10:29:41 +0200

    Rechenweg

--- src/Euklid.java
+++ src/Euklid.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Euklid {
 
   public static Integer getEuklid(int a, int b) {
+    System.out.println("Starting GCD calculation for " + a + " and " + b);
+
+    // Continue the loop until a and b are equal
     while (a != b) {
       if (a > b) {
+        System.out.println(a + " - " + b + " = " + (a - b)); // Show the step
         a = a - b;
       } else {
+        System.out.println(b + " - " + a + " = " + (b - a)); // Show the step
         b = b - a;
       }
     }
-    return a;
+
+    System.out.println("GCD found: " + a);
+    return a; // Return the greatest common divisor
   }
 
   public static void main(String[] args) {
@@ -22,10 +29,9 @@ public class Euklid {
     System.out.println("Enter second number: ");
     int b = sc.nextInt();
 
+    // Call the getEuklid method and print the result
     System.out.println("GCD of " + a + " and " + b + ": " + Euklid.getEuklid(a, b).toString());
 
     sc.close();
-
   }
 }
-
