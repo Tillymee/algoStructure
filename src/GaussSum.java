d8b72bd6f9d1a8d2bd6fc2ef15ce71d13b080961
tillymee <tilly.zech70@gmail.com>
2024-09-30 11:19:45 +0200

    GaussSum LE2

---
+++ src/GaussSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class GaussSum {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ganze Zahl n: ");
+        int n = input.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i; // sum = sum + i
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
