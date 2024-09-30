2d77e36ceb45db6b96ac3702634040a817fe8100
tillymee <tilly.zech70@gmail.com>
2024-09-30 11:22:12 +0200

    GaussSum und NamenArray LE2

---
+++ src/Lerneinheit2/NamenArray.java
@@ -0,0 +1,13 @@
+package Lerneinheit2;
+
+public class NamenArray {
+    public static void main(String[] args) {
+
+        String[] namen = {"Abdelrhman", "Manfouo", "Köhn", "Rayya", "Khachan"};
+
+        // Iteriere über das Array und gebe die Namen aus
+        for (int i = 0; i < namen.length; i++) {
+            System.out.println(namen[i]);
+        }
+    }
+}
